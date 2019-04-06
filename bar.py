935da7e5938a44646a01d595f5f52840c2edbc47
Muhammed Uluel <muhammeduluel@outlook.com>
2019-04-06 20:43:41 +0300

    explonation about 'print('')' in my code

--- bar.py
+++ bar.py
@@ -1,8 +1,8 @@
 def chart(score):
     for i in range(score):
         print('#',end='')
-    print('\n')
-
+    print('') #this is for the go down a line
+    
 for i in range(1,4):
     score=int(input(f'Score {i}: '))
     print(f'Score {i}: ',end='')
