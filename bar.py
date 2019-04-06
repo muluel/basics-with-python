307fce77e5316e7d94faed136a27f02a1eae1b8b
Muhammed Uluel <muhammeduluel@outlook.com>
2019-04-06 21:03:11 +0300

    chart function fixed

--- bar.py
+++ bar.py
@@ -1,8 +1,7 @@
 def chart(score):
-    for i in range(score):
-        print('#',end='')
+    print('#'*score,end='')
     print('') #this is for the go down a line
-    
+
 for i in range(1,4):
     score=int(input(f'Score {i}: '))
     print(f'Score {i}: ',end='')
