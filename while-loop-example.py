eea6540d2a40fd1fa223e5915bc4ac6ad00d23ba
Muhammed Uluel <muhammeduluel@outlook.com>
2019-04-06 21:10:01 +0300

    code fixed

--- while-loop-example.py
+++ while-loop-example.py
@@ -3,13 +3,14 @@ guess = ''
 guess_count = 0
 guess_limit = 3
 out_of_guesses = False
+
 while guess != secret_word and not (out_of_guesses):
     if guess_count < guess_limit:
         guess = input('What is your guess?:')
-        guess_count += 1 
     else:
         out_of_guesses = True
         break
+    guess_count += 1 
 
 if out_of_guesses:
     print('Out of Guessess!\nGame Over!')
